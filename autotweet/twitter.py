c2da9d1bf7ec8ae8fb9b1f3e9974ab8c991968ac
kjwon15 <kjwonmail@gmail.com>
2014-06-18 23:38:45 +0900

    Unescape html entities.

--- autotweet/twitter.py
+++ autotweet/twitter.py
@@ -1,6 +1,10 @@
 import re
 import tweepy
 import webbrowser
+try:
+    from HTMLParser import HTMLParser
+except ImportError:
+    from html.parser import HTMLParser
 
 
 CONSUMER_KEY = '62yWrV2RhpGgWOKlqvJPNQ'
@@ -8,6 +12,7 @@ CONSUMER_SECRET = 'Je6NLI7AN3c1BJP9kHaq1p8GBkMyKs5GhX954dWJ6I'
 
 url_pattern = re.compile(r'https?://[^\s]+')
 mention_pattern = re.compile(r'@\w+')
+html_parser = HTMLParser()
 
 
 def authorize():
@@ -26,5 +31,6 @@ def strip_tweet(text, remove_url=True):
     if remove_url:
         text = url_pattern.sub('', text)
     text = mention_pattern.sub('', text)
+    text = html_parser.unescape(text)
     text = text.strip()
     return text
