a3f599f87f37a3c7cab97d0409f7d60a798158d1
kjwon15 <kjwonmail@gmail.com>
2014-07-11 02:01:36 +0900

    Test code.

--- tests/answer_test.py
+++ tests/answer_test.py
@@ -1,11 +1,13 @@
-from autotweet.database import add_document, get_best_answer, init_db
+from autotweet.database import AutoAnswer
 
 
 def test_answers():
-    session = init_db('sqlite:///:memory:')
-    add_document(session, u'I like yummy cake', u'yummy cake')
-    add_document(session, u'I like scary cake', u'scary cake')
+    aa = AutoAnswer('sqlite:///:memory:')
+    aa.add_document(u'I like yummy cake', u'yummy cake')
+    aa.add_document(u'I like scary cake', u'scary cake')
 
-    assert get_best_answer(session, u'yummy pie') == u'yummy cake'
-    assert get_best_answer(session, u'scary pie') == u'scary cake'
-    assert not get_best_answer(session, u'blabla')
+    answer, ratio = aa.get_best_answer(u'yummy pie')
+    assert answer == u'yummy cake'
+    answer, ratio = aa.get_best_answer(u'scary pie')
+    assert answer == u'scary cake'
+    assert not aa.get_best_answer(u'blabla')
