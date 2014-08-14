ba7bb38bc3d4ec75ca539fd2c56095acccd45e4e
kjwon15 <kjwonmail@gmail.com>
2014-08-15 00:58:29 +0900

    Fix tests

--- tests/answer_test.py
+++ tests/answer_test.py
@@ -1,5 +1,6 @@
 from pytest import fixture
-from autotweet.database import (add_document, get_session, get_best_answer)
+from autotweet.database import (add_document, get_count, get_session,
+                                get_best_answer)
 
 
 @fixture
@@ -19,3 +20,13 @@ def test_answers(fx_session):
     answer, ratio = get_best_answer(session, u'scary pie')
     assert answer == u'scary cake'
     assert not get_best_answer(session, u'blabla')
+
+
+def test_count(fx_session):
+    session = fx_session
+    assert get_count(session) == 0
+
+    add_document(session, u'this is an apple.', u'I like apple.')
+    assert get_count(session) == 1
+    add_document(session, u'this is an apple.', u'I like apple.')
+    assert get_count(session) == 1
