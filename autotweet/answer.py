2069fd8936354d9b8460b39c532083d600f843a8
kjwon15 <kjwonmail@gmail.com>
2014-09-24 23:51:54 +0900

    Typo

--- autotweet/answer.py
+++ autotweet/answer.py
@@ -104,7 +104,8 @@ def polling_timeline(api, db_url, threshold=None):
         else:
             statuses = api.home_timeline(since_id=last_id)
 
-        statuses = filter(lambda x: 'retweeted_status' not in status, statuses)
+        statuses = filter(lambda x: not hasattr(x, 'retweeted_status'),
+                          statuses)
 
         if statuses:
             statuses.reverse()
