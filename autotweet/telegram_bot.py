e4c4a0ed07e617221ce16bde6329cc139476ade1
kjwon15 <kjwonmail@gmail.com>
2016-11-20 01:22:03 +0900

    Check necessary to reply

--- autotweet/telegram_bot.py
+++ autotweet/telegram_bot.py
@@ -6,6 +6,8 @@ This module provides abillities to connect to telegram.
 """
 from __future__ import unicode_literals
 
+import re
+
 from telegram.ext import (
     BaseFilter, CommandHandler, Filters, MessageHandler, Updater)
 
@@ -48,14 +50,17 @@ class TelegramBot(object):
         self.data_collection.add_document(question, answer)
 
     def answering_handler(self, bot, update):
-        question = update.message.text
+        question = strip_tweet(update.message.text)
         try:
             answer, ratio = self.data_collection.get_best_answer(question)
-            if ratio > self.threshold:
+            if (ratio > self.threshold or
+                    self._is_necessary_to_reply(bot, update)):
                 logger.info('{} -> {}'.format(question, answer))
                 update.message.reply_text(
                     answer, quote=True)
         except NoAnswerError:
+            if self._is_necessary_to_reply(bot, update):
+                update.message.reply_text(r'¯\_(ツ)_/¯')
             logger.debug('No answer to {}'.format(question))
 
     def leave_handler(self, bot, update):
@@ -78,6 +83,23 @@ class TelegramBot(object):
 
     def _init_handlers(self):
         self.dispatcher.add_handler(CommandHandler('leave', self.leave_handler))
+
+    @staticmethod
+    def _is_necessary_to_reply(bot, update):
+        message = update.message
+
+        if message.chat.type == 'private':
+            logger.debug('{} type private'.format(message.text))
+            return True
+
+        my_name = bot.get_me().username
+        matched = re.search('@{}\b'.format(my_name), message.text)
+        result = bool(matched)
+        if result:
+            logger.debug('{} mentioned me.'.format(message.text))
+            return True
+
+        return False
 
 
 def start_bot(token, db_uri, threshold, learning=True, answering=True):
